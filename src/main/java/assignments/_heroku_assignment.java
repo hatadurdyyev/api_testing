a39e3890576409f497b9018468a4f6e6d2e2d17e
CatsHaveFeet <and.surubariu@gmail.com>
2024-02-12 16:31:37 +0200

    finish get assertion

--- src/main/java/assignments/_heroku_assignment.java
+++ src/main/java/assignments/_heroku_assignment.java
@@ -1,9 +1,13 @@
 package assignments;
 
 import get_urls.herokuPlaceHolderUrl;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -11,16 +15,54 @@ import static org.junit.Assert.assertEquals;
 import static test_data.heroku_PlaceHolderTestData.bookingMapMethod;
 import static test_data.heroku_PlaceHolderTestData.expectedDataMapMethod;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class _heroku_assignment extends herokuPlaceHolderUrl {
-    @Test
-    public void post() {
-        spec.pathParams("first", "booking", "second", 455);
-        Map<String, String> bookingdates = bookingMapMethod("2024-04-07", "2024-04-27");
-        Map<String, Object> expectedData = expectedDataMapMethod("John", "Kaszynski", 499, false, bookingdates, "Cookies");
 
-        Response response = given(spec).body(expectedData).when().post("{first}/{second}");
+    public static int bookingId;
+
+    public static Map<String, String> bookingdates = bookingMapMethod("2024-04-07", "2024-04-27");
+    public static Map<String, Object> expectedData = expectedDataMapMethod("John", "Kaszynski", 499, false, bookingdates, "Cookies");
+
+    @Test
+    public void Apost() {
+        spec.pathParam("first", "booking");
+
+        Response response = given(spec).body(expectedData).when().post("{first}");
         response.prettyPrint();
 
+        JsonPath jsonPath = response.jsonPath();
+
+        bookingId = jsonPath.getInt("bookingid");
+        //System.out.println(bookingId);
+        Map<String, Object> actualData = jsonPath.getMap("booking");
+
         assertEquals(200, response.statusCode());
+        assertEquals(expectedData.get("firstname"), actualData.get("firstname"));
+        assertEquals(expectedData.get("lastname"), actualData.get("lastname"));
+        assertEquals(expectedData.get("totalprice"), actualData.get("totalprice"));
+        assertEquals(expectedData.get("depositpaid"), actualData.get("depositpaid"));
+        assertEquals(((Map<String, Object>)expectedData.get("bookingdates")).get("checkin"), ((Map<String, Object>)actualData.get("bookingdates")).get("checkin"));
+        assertEquals(((Map<String, Object>)expectedData.get("bookingdates")).get("checkout"), ((Map<String, Object>)actualData.get("bookingdates")).get("checkout"));
+        assertEquals(expectedData.get("additionalneeds"), actualData.get("additionalneeds"));
+    }
+
+    @Test
+    public void Bget() {
+        //System.out.println(bookingId);
+        spec.pathParams("first", "booking", "second", bookingId);
+
+        Response response = given(spec).when().get("{first}/{second}");
+        response.prettyPrint();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Map<String, Object> actualData = response.as(HashMap.class);
+
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.get("firstname"), actualData.get("firstname"));
+        assertEquals(expectedData.get("lastname"), actualData.get("lastname"));
+        assertEquals(expectedData.get("totalprice"), actualData.get("totalprice"));
+        assertEquals(expectedData.get("depositpaid"), actualData.get("depositpaid"));
+        assertEquals(expectedData.get());
     }
 }
